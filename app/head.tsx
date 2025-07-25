18e4ae08eca07ba688da2a4c1caf9b52f2fc714f
yuan-miranda <yuanezekielamiranda@gmail.com>
2025-07-25 15:23:46 +0800

    fixed the meta thing, removed the head.tsx

--- app/head.tsx
+++
@@ -1,9 +0,0 @@
-// app/head.tsx
-export default function Head() {
-    return (
-        <>
-            <meta charSet="UTF-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-        </>
-    );
-}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 // app/layout.tsx
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -16,6 +16,13 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
 	title: "Anarctism",
 	description: "React Fabric.js implementation of Anarctist",
+};
+
+export const viewport: Viewport = {
+	width: "device-width",
+	initialScale: 1,
+	maximumScale: 1,
+	userScalable: false,
 };
 
 export default function RootLayout({
