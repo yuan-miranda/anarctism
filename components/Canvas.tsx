8dc2dc42a5f8b9003c20c3dd7af4c2ec97e387c5
yuan-miranda <yuanezekielamiranda@gmail.com>
2025-07-29 21:24:59 +0800

    reverted

--- components/Canvas.tsx
+++ components/Canvas.tsx
@@ -135,7 +135,8 @@ export default function Canvas() {
             const dx = e.touches[0].clientX - panStart.x;
             const dy = e.touches[0].clientY - panStart.y;
 
-            container.style.transform = `translate(${containerStart.left + dx}px, ${containerStart.top + dy}px)`;
+            container.style.left = (containerStart.left + dx) + 'px';
+            container.style.top = (containerStart.top + dy) + 'px';
         };
 
         const onTouchEnd = (e: TouchEvent) => {
